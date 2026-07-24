fb6d0f76a78e49d52358d310b5e8d2e2fc144982
omarkrimarbi3-ai <omarkrimarbi3@gmail.com>
2026-07-24 15:06:32 +0100

    Update surprise.js

--- surprise.js
+++ surprise.js
@@ -2,16 +2,8 @@ const result = document.getElementById("result");
 const nextBtn = document.getElementById("nextBtn");
 
 function showSurprise() {
-
     const random = Math.floor(Math.random() * surprises.length);
-
-    result.classList.remove("showAnimation");
-
-    void result.offsetWidth;
-
     result.textContent = surprises[random];
-
-    result.classList.add("showAnimation");
 }
 
         result.style.opacity = "1";
