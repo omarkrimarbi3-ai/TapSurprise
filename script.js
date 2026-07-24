c1a287d9419325d10949a1e507561f7f2c25d94d
omarkrimarbi3-ai <omarkrimarbi3@gmail.com>
2026-07-24 13:55:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+window.onload = function () {
+
 const button = document.getElementById("surpriseBtn");
 const result = document.getElementById("result");
 
-button.addEventListener("click", () => {
-    const random = Math.floor(Math.random() * surprises.length);
-    result.textContent = surprises[random];
+button.addEventListener("click", function () {
+
+const random = Math.floor(Math.random() * surprises.length);
+
+result.textContent = surprises[random];
+
 });
+
+};
